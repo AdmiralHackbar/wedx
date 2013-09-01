a781192b4b8b97795062085864f420b95cae6e1c
Daniel Gephardt <the.admiral.hackbar@gmail.com>
2013-09-01 16:48:38 -0500

    Adding index page template and base template.

--- weddingredux/views/indexview.py
+++ weddingredux/views/indexview.py
@@ -1,6 +1,13 @@
 __author__ = 'dgg1988'
 
 from django.http import HttpResponse
+from django.template import RequestContext, loader
 
 def index(request):
-    return HttpResponse("This will be the index view.")
+    template = loader.get_template("index.html")
+    context = RequestContext(request, {
+        'page' : {
+            'title' : "WAKAKAKAKAKAKA"
+        }
+    })
+    return HttpResponse(template.render(context))
